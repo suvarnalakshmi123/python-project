4dc5416662cc20f93a216a41e2ef6098833bcafc
suvarnalakshmi123 <150534753+suvarnalakshmi123@users.noreply.github.com>
2024-03-29 20:59:06 +0530

    Update display_board.py

--- display_board.py
+++ display_board.py
@@ -4,3 +4,10 @@ def display_board():
       print("|    |    |    |\n|    |    |    |\n|    |    |    |")
       print("+----+----+----+")
 display_board()
+def make_list_of_free_fields(board):
+    free = []
+    for i in range(3): 
+        for j in range(3):
+            if board[i][j] not in ['X', 'O']:
+                free.append((i, j))
+    return free()
