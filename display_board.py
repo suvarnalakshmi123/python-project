4b81ba8c38c1035061671f0fe600362c494fba6b
suvarnalakshmi123 <150534753+suvarnalakshmi123@users.noreply.github.com>
2024-03-29 19:57:39 +0530

    Add files via upload

---
+++ display_board.py
@@ -0,0 +1,6 @@
+def display_board():
+  print("+----+----+----+")
+  for i in range(3):
+      print("|    |    |    |\n|    |    |    |\n|    |    |    |")
+      print("+----+----+----+")
+display_board()
